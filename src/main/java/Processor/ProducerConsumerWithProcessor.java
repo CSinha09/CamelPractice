c9e557673f1ba93c327250e546116bdbbf1756e9
Chirag Sinha <chirag.sinha@zeotap.com>
2023-01-27 11:35:32 +0530

    Processor Implementation

---
+++ src/main/java/Processor/ProducerConsumerWithProcessor.java
@@ -0,0 +1,44 @@
+package Processor;
+
+import org.apache.camel.*;
+import org.apache.camel.builder.ProcessClause;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class ProducerConsumerWithProcessor {
+
+    public static void main(String[] args) throws Exception {
+
+        CamelContext context = new DefaultCamelContext();
+
+        context.addRoutes(new RouteBuilder() {
+
+            @Override
+            public void configure() throws Exception {
+                from("direct:start")
+                        .process(new Processor() {
+                            @Override
+                            public void process(Exchange exchange) throws Exception {
+                                System.out.println("Hello from the processor");
+                            }
+                        })
+                        .to("seda:end");
+            }
+
+        });
+
+        context.start();
+
+        ProducerTemplate producerTemplate = context.createProducerTemplate();
+
+        producerTemplate.sendBody("direct:start", "Hello from the producer");
+
+        ConsumerTemplate consumerTemplate = context.createConsumerTemplate();
+
+        String msg = consumerTemplate.receiveBody("seda:end", String.class);
+
+        System.out.println(msg);
+
+    }
+
+}
